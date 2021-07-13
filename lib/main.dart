b3ca10e3e6417bc80476b8190d822bf5cdb2d548
Cristopher González Rodríguez <criss9rodri@example.com>
2021-07-13 07:44:44 -0600

    vista de estudiantes  e inserción de actividades

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
+import 'package:commitem_proyect/pages/Actividades.dart';
 import 'package:commitem_proyect/pages/AdminPage.dart';
 import 'package:commitem_proyect/pages/CoordinadorPage.dart';
 import 'package:commitem_proyect/pages/EstudiantePage.dart';
+import 'package:commitem_proyect/pages/InsertarActividad.dart';
 import 'package:commitem_proyect/pages/ProfesorPage.dart';
 import 'package:flutter/material.dart';
 import 'package:commitem_proyect/pages/login_screen.dart';
@@ -20,6 +22,8 @@ class MyApp extends StatelessWidget {
         '/CoordinadorPage': (BuildContext context) => new Coordinador(),
         '/ProfesorPage': (BuildContext context) => new Profesor(),
         '/loginPage': (BuildContext context) => LoginScreen(),
+        '/ActividadesPage': (BuildContext context) => Actividades(),
+        '/insertarActividad': (BuildContext context) => InsertarActividad(),
       },
     );
   }
--- lib/pages/EstudiantePage.dart
+++ lib/pages/EstudiantePage.dart
@@ -1,20 +1,103 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
-class Estudiante extends StatelessWidget {
+class Estudiante extends StatefulWidget {
+  @override
+  _EstudianteState createState() => _EstudianteState();
+}
+
+class _EstudianteState extends State<Estudiante> {
+  List _dataEstudiante = List();
+
+  void _ambilData() async {
+    final res = await http.post(
+      Uri.parse(
+          "https://commitem.000webhostapp.com/appcommitem/consultarEstudiante.php"),
+    );
+    setState(() {
+      _dataEstudiante = json.decode(res.body);
+    });
+  }
+
+  @override
+  void initState() {
+    _ambilData();
+    super.initState();
+  }
+
+  SingleChildScrollView _tabelEstudiante() {
+    return SingleChildScrollView(
+      scrollDirection: Axis.vertical,
+      child: SingleChildScrollView(
+        scrollDirection: Axis.horizontal,
+        child: DataTable(
+            columns: [
+              DataColumn(label: Text("Carnet")),
+              DataColumn(label: Text("Proyecto")),
+              DataColumn(label: Text("Nombre")),
+              DataColumn(label: Text("Apellido1")),
+              DataColumn(label: Text("Apellido2")),
+              DataColumn(label: Text("Telefono")),
+              DataColumn(label: Text("Correo")),
+              DataColumn(label: Text("Carrera")),
+              DataColumn(label: Text("Horas")),
+              DataColumn(label: Text("Estado")),
+            ],
+            rows: _dataEstudiante
+                .map((item) => DataRow(cells: <DataCell>[
+                      DataCell(Text(item["estudianteTCU_carne"])),
+                      DataCell(Text(item["proyectoTCU_id"])),
+                      DataCell(Text(item["estudianteTCU_nombre"])),
+                      DataCell(Text(item["estudianteTCU_apellido1"])),
+                      DataCell(Text(item["estudianteTCU_apellido2"])),
+                      DataCell(Text(item["estudianteTCU_tel"])),
+                      DataCell(Text(item["estudianteTCU_correoIns"])),
+                      DataCell(Text(item["estudianteTCU_carrera"])),
+                      DataCell(Text(item["estudianteTCU_totalHoras"])),
+                      DataCell(Text(item["estudianteTCU_estado"])),
+                    ]))
+                .toList()),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: new AppBar(title: Text ('Pagina Estudiantes'),),
-      body: new Column(
-        children: <Widget>[
-          new Text ('Estamos en Page Estudiante'),
-            FloatingActionButton(
-            child: Text("Salir"),
+      appBar: AppBar(
+        title: Text("Estudiante"),
+      ),
+      body: Container(
+        width: MediaQuery.of(context).size.width,
+        padding: const EdgeInsets.all(8),
+        child: ListView(children: <Widget>[
+          Text(
+            "Datos del Estudiante",
+            style: TextStyle(fontSize: 19),
+          ),
+          SizedBox(
+            height: 15,
+          ),
+          _tabelEstudiante(),
+          FlatButton(
+            onPressed: (){
+              Navigator.pushReplacementNamed(context, '/ActividadesPage');
+            },
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(30)),
+            color: Colors.blue[700],
+            child: Text("Actividades", style: TextStyle(
+              fontSize: 20.0,
+              fontWeight: FontWeight.bold
+            ),),),
+          FloatingActionButton(
+            child: Icon(Icons.backspace),
             onPressed: () {
               Navigator.pushReplacementNamed(context, '/loginPage');
             },
           )
-        ],
+        ]),
       ),
     );
   }
